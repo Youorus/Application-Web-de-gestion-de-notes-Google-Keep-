4d4795d7bbcefb8ea486fabbe02d62c945f87a98
Yasmina Riouch <yasmina.riouch02@gmail.com>
2023-12-04 13:48:29 +0100

    creation model user

---
+++ model/User.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "framework/Model.php";
+
+class User extends Model{
+
+    public function __construct(public string $mail, public string $hashed_password, public string $name, public String $role){
+
+    }
+
+
+    public static function get_user_by_mail(string $mail) : User|false {
+        $query = self::execute("SELECT * FROM Users where mail = :mail", ["mail"=>$mail]);
+        $data = $query->fetch(); // un seul résultat au maximum
+        if ($query->rowCount() == 0) {
+            return false;
+        } else {
+            return new User($data["mail"], $data["hashed_password"], $data["full_name"], $data["role"]);
+        }
+    }
+    private static function check_password(string $clear_password, string $hash) : bool {
+        return $hash === Tools::my_hash($clear_password);
+    }
+
+    public static function validate_login(string $mail, string $password) : array {
+        $errors = [];
+        $user = User::get_user_by_mail($mail);
+        if ($user) {
+            if (!self::check_password($password, $user->hashed_password)) {
+                $errors[] = "Wrong password. Please try again.";
+            }
+        } else {
+            $errors[] = "Can't find a User with the email '$mail'. Please sign up.";
+        }
+        return $errors;
+    }
+}
