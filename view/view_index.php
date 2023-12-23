91d4ca076d5aa5214a85a97ba0b80ed6069a6940
Marc Takoumba <mtakoumba@gmail.com>
2023-12-23 16:14:47 +0100

    feat(view_notes): factorisation du code de la vue des notes

--- view/view_index.php
+++ view/view_index.php
@@ -88,53 +88,15 @@
 <div class="container">
     <h3>Pinned</h3>
     <div class="row">
+       <?php
+       include "utils/pinnedNote.php";
+       ?>
+    </div>
+    <br>
+    <h3>Others</h3>
+    <div class="row">
         <?php
-        for ($i = 0; $i < count($notesPinned); $i++) {
-            echo '<div class="col-md-4">';
-            echo '<div class="card">';
-            echo '<div class="card-body">';
-            echo '<h5 class="card-title bord">' . $notesPinned[$i]->getTitle() . '</h5>';
-
-          if ($notesPinned[$i]->getType() == NoteType::TextNote) {
-                if ($notesPinned[$i]->getContent() === null) {
-                    echo '<p class="card-text">' . 'Note vide' . '</p>';
-                }else {
-                    $length_string = strlen($notesPinned[$i]->getContent());
-                    if ($length_string > 80) {
-                        $tmp_string = "";
-                        for ($j = 0; $j < $length_string; $j++) {
-                            $tmp_string .= $notesPinned[$i]->getContent()[$j];
-                            if ($j == 80) {
-                                break;
-                            }
-                        }
-                       echo '<p class="card-text">' . $tmp_string . "..." . '</p>';
-                    } else {
-                        echo '<p class="card-text">' . $notesPinned[$i]->getContent() . '</p>';
-                    }
-                }
-               }else{
-              $items = $notesPinned[$i]->getItems();
-              foreach ($items as $item) {
-                  echo '<div class="form-check">';
-               echo '  <input class="form-check-input" type="checkbox" value="1" id="exampleCheckbox" name="exampleCheckbox" ' . ($item ->getChecked() == 1 ? 'checked' : '') . '>';
-                echo '  <label class="form-check-label" for="exampleCheckbox">';
-               echo $item->getContent();
-               echo '  </label>';
-                echo '</div>';
-              }
-              }
-
-            echo '</div>';
-            echo '<div class="d-flex iconBox justify-content-between">';
-            echo '<span><i class="fa-solid fa-angles-left"></i></span>';
-            echo '<span><i class="fa-solid fa-angles-right"></i></span>';
-            echo '</div>';
-            echo '</div>';
-            echo '</div>';
-
-        }
-
+        include "utils/othersNote.php";
         ?>
     </div>
 </div>
