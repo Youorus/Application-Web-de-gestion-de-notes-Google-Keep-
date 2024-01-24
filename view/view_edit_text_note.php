c89c0aadc7ca641dc32a2aedf9d4dae9d62f3854
Yasmina Riouch <yasmina.riouch02@gmail.com>
2024-01-24 17:46:56 +0100

    feat_edit_text_note :modification de la vue

--- view/view_edit_text_note.php
+++ view/view_edit_text_note.php
@@ -8,7 +8,28 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="stylesheet" href="css/style.css">
 
+
+  <style>
+        .mb-3{
+            margin: 4% 5% 0 5%;
+        }
+        .infoText{
+            font-style: italic;
+            font-weight: normal;
+            font-size: initial;
+            margin-bottom: 2%;
+        }
+        .form-label{
+            font-size: larger;
+        }
+        .form-control {
+            background-color: var(--couleur-bordure) !important;
+            color: var(--couleur-texte);
+            border-color: var(--couleur-bordure);
+        }
+    </style>
 </head>
     <body>
     <div class="offcanvas offcanvas-start navbar-dark bg-dark text-bg-dark show" style="width:100vw;" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
@@ -17,12 +38,16 @@
                 <span class="material-symbols-outlined">
                     arrow_back_ios 
                 </span>
-                <span class="material-symbols-outlined" style="margin-left: auto;">
+                </i>
+                <i class="material-icons" style="margin-left: auto;">
+                <span class="material-symbols-outlined">
                     save_as
                 </span>
                 </i>
 
-            </div>    
+            </div>   
+            
+            <!-- la je dois rajouter du php pour afficher la date de création et d'edition de la note (infosText)-->
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" class="form-control" id="title" >
@@ -36,3 +61,5 @@
     </body>
 
 </html>    
+
+
