40444e3e823568e943e4007d287e15426bff2dfb
Marc Takoumba <mtakoumba@gmail.com>
2024-04-03 11:58:05 +0200

    gestion de la validation de addTextNote avec JS

--- controller/ControllerAddTextNote.php
+++ controller/ControllerAddTextNote.php
@@ -7,16 +7,11 @@ class ControllerAddTextNote extends Controller
 {
     public function index(): void
     {
-        (new View("add_text_note"))->show([]);
-    }
-
-    public function add_text_note(): void
-    {
+        $user = $this->get_user_or_redirect();
         $errors = []; // Tableau pour stocker les erreurs
         $title_note = '';
         $content_note = '';
         $idnote = "";
-        $user = $this->get_user_or_redirect();
 
         if (isset($_POST["title_note"]) && isset($_POST["content_note"])) {
             $title_note = trim($_POST["title_note"]);
@@ -24,11 +19,11 @@ class ControllerAddTextNote extends Controller
 
             // Validation de la longueur du titre
 
-                $errors[] = TextNote::validate($title_note);
+            $errors[] = TextNote::validate($title_note);
 
-                if($user->title_exist( $title_note)){
-                    $errors[] = "this title is already exist";
-                }
+            if($user->title_exist( $title_note)){
+                $errors[] = "this title is already exist";
+            }
 
 
             if (empty($errors)){
@@ -41,7 +36,7 @@ class ControllerAddTextNote extends Controller
 
                 // Redirection vers l'index avec l'ID de la note en tant que paramètre
                 $idnote = $note->getId();
-              $this->redirect("index", "open_text_note", "$idnote");
+                $this->redirect("index", "open_text_note", "$idnote");
             }
 
         }
@@ -50,14 +45,57 @@ class ControllerAddTextNote extends Controller
         (new View("add_text_note"))->show(["errors" => $errors, "title_note" => $title_note, "content_note" => $content_note]);
     }
 
+//    public function add_text_note(): void
+//    {
+//
+//        $user = $this->get_user_or_redirect();
+//        $errors = []; // Tableau pour stocker les erreurs
+//        $title_note = '';
+//        $content_note = '';
+//        $idnote = "";
+//
+//        if (isset($_POST["title_note"]) && isset($_POST["content_note"])) {
+//            $title_note = trim($_POST["title_note"]);
+//            $content_note = trim($_POST["content_note"]);
+//
+//            // Validation de la longueur du titre
+//
+//                $errors[] = TextNote::validate($title_note);
+//
+//                if($user->title_exist( $title_note)){
+//                    $errors[] = "this title is already exist";
+//                }
+//
+//
+//            if (empty($errors)){
+//                // Création de la note si la validation est réussie
+//                $note = new TextNote(0, $content_note);
+//                $note->setOwner($user->getId());
+//                $note->setTitle($title_note);
+//                $note->persist(); // Enregistrement de la note
+//
+//
+//                // Redirection vers l'index avec l'ID de la note en tant que paramètre
+//                $idnote = $note->getId();
+//              $this->redirect("index", "open_text_note", "$idnote");
+//            }
+//
+//        }
+//
+//        // Affichage de la vue avec les erreurs
+//        (new View("add_text_note"))->show(["errors" => $errors, "title_note" => $title_note, "content_note" => $content_note]);
+//    }
 
-    public function validate(): bool{
-        $res = false;
-        if (isset($_GET["param1"]) && $_GET["param1"] !== ""){
-            $title = $_GET["param1"];
-            $res = TextNote::validate($title);
+
+    public function validate(): void{
+        $user = $this->get_user_or_redirect();
+        $res = "false";
+        if (isset($_POST["test"])){
+            if($user->title_exist($_POST["test"]))
+                $res = "true";
+
         }
-        return $res;
+        echo $res;
     }
 }
 ?>
